fda7be29a44ea3db0a6a3db49c45c03098b97655
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-05-02 13:11:00 +0700

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -46,7 +46,6 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    // resultsView.render(model.state.search.results)
     resultsView.render(model.getSearchResultsPage());
 
     // 4) Render initial pagination button
@@ -58,7 +57,6 @@ const controlSearchResults = async function () {
 
 const controlPagination = function (goToPage) {
   // 1) Render NEW results
-  // resultsView.render(model.state.search.results)
   resultsView.render(model.getSearchResultsPage(goToPage));
 
   // 2) Render NEW pagination button
@@ -70,7 +68,6 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
@@ -121,10 +118,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -133,7 +126,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
