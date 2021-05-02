206aa9b42c317881d0580914fea3deea4eac94d9
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-05-02 10:22:50 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '55385978-7985-4ecf-a674-3475d68faac1';
+export const MODAL_CLOSE_SEC = 2.5;
